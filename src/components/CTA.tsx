b6db550b44872a786f2a4e4e881dd99b26c451bd
Kyrin <143742919+Paramethia@users.noreply.github.com>
2025-07-07 02:23:26 +0200

    Fucken hell

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -8,8 +8,7 @@ export function CTA() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h2 className="text-3xl md:text-4xl font-bold text-gray-200 mb-4">Ready to Transform Your Music?</h2>
         <p className="text-xl mb-8 max-w-3xl mx-auto text-gray-200">
-          If you're ready to commit to your art, we're ready to help you shape your future in music. Let's create
-          something amazing together.
+          If you&apos;re ready to commit to your art, we&apos;re ready to help you shape your future in music. Let&apos;s create something amazing together.
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
